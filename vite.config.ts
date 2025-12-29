58de7c06f2b3ac5d742d3166fd6d9d9098d043d6
Eric Dai <ericdai2000@gmail.com>
2025-12-29 17:45:45 -0500

    Remove formulizePlugin feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { formulizePlugin } from "formulize-math/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [formulizePlugin(), react()],
+  plugins: [react()],
 });
