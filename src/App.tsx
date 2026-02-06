6583f3cdb1fde43c6a77d6d4296fe5f0066a1915
Eric Dai <ericdai2000@gmail.com>
2026-02-06 13:41:05 -0500

    Added training example

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,55 @@
 import {
   Formula,
   FormulizeProvider,
-  StepControl,
   type FormulizeConfig,
 } from "formulize-math";
 
-const averageConfig: FormulizeConfig = {
+const linearConfig: FormulizeConfig = {
   formulas: [
     {
-      id: "average",
-      latex: "\\bar{x} = \\frac{1}{n} \\sum_{i=1}^{n} x_i",
+      id: "linear-equation",
+      latex: "y = mx + b",
     },
   ],
   variables: {
-    "\\bar{x}": {},
-    n: {
+    y: {
       default: 0,
-      name: "Count",
     },
-    i: {
-      name: "Index",
-    },
-    x_i: {
-      name: "Value at i",
+    m: {
+      input: "drag",
+      default: 2,
+      range: [-5, 5],
+      step: 0.5,
+      name: "Slope",
     },
     x: {
-      default: [10, 20, 30, 40, 50],
-      name: "Data values",
+      input: "drag",
+      default: 3,
+      range: [-10, 10],
+      step: 0.5,
+    },
+    b: {
+      input: "drag",
+      default: 1,
+      range: [-10, 10],
+      step: 0.5,
+      name: "Y-Intercept",
     },
   },
-  stepping: true,
-  semantics: ({ vars, step }) => {
-    const xValues = vars.x as number[];
-    const n = xValues.length;
-    let sum = 0;
-    step({
-      description: "Get the count $n$ of values",
-      values: [["n", n]],
-    });
-    for (let i = 0; i < n; i++) {
-      const xi = xValues[i];
-      sum = sum + xi;
-    }
-    let average = sum / n;
-    average = Math.round(average * 100) / 100;
-    step({
-      description:
-        "Divide $\\sum = " + sum + "$ by $n = " + n + "$ to get average",
-      values: [["\\bar{x}", average]],
-    });
-    vars["\\bar{x}"] = average;
+  semantics: function ({ vars, data2d }) {
+    vars.y = vars.m * vars.x + vars.b;
+    data2d("linear", { x: vars.x, y: vars.y });
   },
-  fontSize: 1.5,
 };
 
 function App() {
   return (
-    <FormulizeProvider config={averageConfig}>
+    <FormulizeProvider config={linearConfig}>
       <div className="min-h-screen p-8 bg-gray-50 flex flex-col items-center">
-        <Formula id="average" style={{ height: "300px", width: "700px" }} />
-        <div className="w-[270px] mx-auto p-4">
-          <StepControl className="w-[700px] mx-auto" />
-        </div>
+        <Formula
+          id="linear-equation"
+          style={{ height: "300px", width: "700px" }}
+        />
       </div>
     </FormulizeProvider>
   );
