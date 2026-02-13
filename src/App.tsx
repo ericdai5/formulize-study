ca129c162a01a7c64d2d7ffcbceaee4254950ccb
Eric Dai <ericdai2000@gmail.com>
2026-02-12 22:13:55 -0500

    Center the example

--- src/App.tsx
+++ src/App.tsx
@@ -24,52 +24,50 @@ function App() {
   const latex = `\\cssId{var-K}{K} = \\frac{1}{2} m \\cssId{var-v}{v}^2`;
 
   return (
-    <div className="min-h-screen p-8 bg-gray-50 flex flex-col items-center">
-      <div className="bg-white p-6 w-full max-w-4xl">
-        <FormulaContainer className="relative">
-          <div className="flex justify-center" style={{ fontSize: "1.5rem" }}>
-            <Formula latex={latex} />
-            <VariableHitArea
-              elementId="var-K"
-              isHovered={kHovered}
-              onHover={setKHovered}
-            />
-            <VariableHitArea
-              elementId="var-v"
-              isHovered={vHovered}
-              onHover={setVHovered}
-              value={v}
-              onChange={setV}
-              min={0.1}
-              max={10}
-              step={0.1}
-            />
-          </div>
-          <div className="flex justify-center gap-8">
-            <VariableLabel
-              elementId="var-K"
-              value={K}
-              unit="J"
-              name="Kinetic Energy"
-              isHovered={kHovered}
-              onHover={setKHovered}
-            />
-            <VariableLabel
-              elementId="var-v"
-              value={v}
-              unit="m/s"
-              name="Velocity"
-              isHovered={vHovered}
-              onHover={setVHovered}
-              onChange={setV}
-              min={0.1}
-              max={10}
-              step={0.1}
-            />
-          </div>
-          <ConnectingLines />
-        </FormulaContainer>
-      </div>
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+      <FormulaContainer className="relative">
+        <div className="flex justify-center" style={{ fontSize: "1.5rem" }}>
+          <Formula latex={latex} />
+          <VariableHitArea
+            elementId="var-K"
+            isHovered={kHovered}
+            onHover={setKHovered}
+          />
+          <VariableHitArea
+            elementId="var-v"
+            isHovered={vHovered}
+            onHover={setVHovered}
+            value={v}
+            onChange={setV}
+            min={0.1}
+            max={10}
+            step={0.1}
+          />
+        </div>
+        <div className="flex justify-center gap-8">
+          <VariableLabel
+            elementId="var-K"
+            value={K}
+            unit="J"
+            name="Kinetic Energy"
+            isHovered={kHovered}
+            onHover={setKHovered}
+          />
+          <VariableLabel
+            elementId="var-v"
+            value={v}
+            unit="m/s"
+            name="Velocity"
+            isHovered={vHovered}
+            onHover={setVHovered}
+            onChange={setV}
+            min={0.1}
+            max={10}
+            step={0.1}
+          />
+        </div>
+        <ConnectingLines />
+      </FormulaContainer>
     </div>
   );
 }
