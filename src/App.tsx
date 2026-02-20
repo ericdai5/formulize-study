97ac98fba8f0f7f8866abb27e247007ed27680fb
Eric Dai <ericdai2000@gmail.com>
2026-02-19 23:14:39 -0500

    Add template for observational study

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,18 @@
-import { Formula, Provider, type Config } from "math-notation";
+import { Formula, Provider, StepControl, type Config } from "math-notation";
 
 const config: Config = {
-  formulas: [
-    {
-      id: "kinetic",
-      latex: "K = \\frac{1}{2}mv^2",
-    },
-  ],
-  variables: {
-    K: {
-      name: "Kinetic Energy",
-    },
-    v: {
-      name: "Velocity",
-      input: "drag",
-      default: 2,
-      range: [0, 100],
-      step: 1,
-    },
-  },
-  semantics: function ({ vars }) {
-    const m = 1;
-    vars.K = 0.5 * m * Math.pow(vars.v, 2);
-  },
-  fontSize: 1.5,
-  labelFontSize: 1.0,
+  formulas: [],
+  variables: {},
+  stepping: true,
+  semantics: function () {},
 };
 
 function App() {
   return (
-    <div className="flex flex-col items-center h-screen">
-      <Provider config={config}>
-        <Formula id="kinetic" style={{ height: "300px", width: "400px" }} />
-      </Provider>
-    </div>
+    <Provider config={config}>
+      <Formula id="" style={{ height: "300px", width: "700px" }} />
+      <StepControl />
+    </Provider>
   );
 }
 
